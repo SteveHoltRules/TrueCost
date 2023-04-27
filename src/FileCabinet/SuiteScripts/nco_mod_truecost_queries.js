4906d586b7d05873c1474e1df01530979207e00e
Matt Luna <matt.c.luna@gmail.com>
2023-04-27 06:17:33 -0500

    create a module for the sql code so that I will have less to compare against for the code fix

---
+++ src/FileCabinet/SuiteScripts/nco_mod_truecost_queries.js
@@ -0,0 +1,22 @@
+/**
+ * @NApiVersion 2.1
+ */
+define(['N/query', 'N/record', 'N/search'],
+    /**
+ * @param{query} query
+ * @param{record} record
+ * @param{search} search
+ */
+    (query, record, search) => {
+
+        const foo = () => {
+
+        }
+
+        const bar = () => {
+
+        }
+
+        return {foo, bar}
+
+    });
